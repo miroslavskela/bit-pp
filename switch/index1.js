efd0e93a6aef292a4bbbbdc16725ddf7b0f6e94d
BIT Student <classroom@bgit.rs>
2018-02-05 10:53:01 +0100

    Switch first commit

---
+++ switch/index1.js
@@ -0,0 +1,16 @@
+var smiley = ":/";
+
+
+switch (smiley) {
+    case ":(":
+    case ":/":
+        console.log("Sad");
+        break;
+
+    case ":)":
+    case ":-)":
+        console.log("Happy");
+        break;
+    default:
+        console.log("Bla");
+}
