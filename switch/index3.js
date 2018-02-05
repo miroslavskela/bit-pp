cdcbf8a236d56decb440781cc8ae81696eb69a64
BIT Student <classroom@bgit.rs>
2018-02-05 11:03:24 +0100

    Switch second commit

---
+++ switch/index3.js
@@ -0,0 +1,27 @@
+var day = 10;
+
+switch (day) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("The number must be between 1 and 7");
+} 
---
+++ switch/index4.js
@@ -0,0 +1,17 @@
+var day = 4;
+
+switch (day) {
+    case 1:   
+    case 2:   
+    case 3:    
+    case 4:   
+    case 5:
+        console.log("It's a weekday.");
+        break;
+    case 6:
+    case 7:
+        console.log("It's weekend.");
+        break;
+    default:
+        console.log("The number must be between 1 and 7.");
+} 
