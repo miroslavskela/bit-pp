3eef30ed41df703e042b9884bb0fa9e168f03409
BIT Student <classroom@bgit.rs>
2018-02-05 11:24:33 +0100

    Swith third commity

---
+++ switch/index5.js
@@ -0,0 +1,27 @@
+var month = 2;
+
+switch (month) {
+    case 1:  
+    case 2: 
+    case 3:
+        console.log("Winter");
+        break;
+    case 4:  
+    case 5:
+    case 6:
+        console.log("Spring");
+        break;
+    case 7:   
+    case 8:
+    case 9:
+        console.log("Summer");
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log("Autumn");
+        break;
+
+    default:
+        console.log("The number must be between 1 and 12");
+} 
