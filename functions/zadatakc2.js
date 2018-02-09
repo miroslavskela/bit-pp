4935ead6d18c365580ea941cd93e535a76e550f5
BIT Student <classroom@bgit.rs>
2018-02-09 14:06:14 +0100

    Functions zadatakc2 konacna verzija

--- functions/zadatakc2.js
+++ functions/zadatakc2.js
@@ -3,20 +3,27 @@
 */
 
 
+/*Write a program to join all elements of the array into a string skipping elements that are undefined, null, NaN or Infinity.
+[NaN, 0, 15, false, -22, '', undefined, 47, null]
+*/
+
+
 function joinElements(arr) {
 
     var niz = "";
 
     for (i = 0; i < arr.length; i++) {
 
-        if ( arr[i] !== undefined && !!arr[i]) {
+        if (isFinite(arr[i])===false || arr[i] === undefined || arr[i] === null) {
 
-            niz+=arr[i];
-        }
+            continue;
+
+        } 
+            niz += arr[i] + " ";
+        
 
     }
     return niz;
-    
+
 }
 console.log(joinElements([NaN, 0, 15, false, -22, '', undefined, 47, null, Infinity]));
-© 2018 GitHub, Inc.
