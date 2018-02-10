57854a654179eba57edf53e3b7c83fe8a94e8f45
miroslavskela <miki.skela90@gmail.com>
2018-02-10 15:12:28 +0100

    Zadaci 4 i 5 sa  trecih vezbi funkcije

--- functions/zadatak1.js
+++ functions/zadatak1.js
@@ -1,9 +1,9 @@
-function max(a, b){
-   if (a < b){
-       return b;
-   }else  {
-       return a;
-   }
+function max(a, b) {
+    if (a < b) {
+        return b;
+    } else {
+        return a;
+    }
 }
 var result = max(55, 55);
 console.log(result);
---
+++ functions/zadatakc4.js
@@ -0,0 +1,19 @@
+//Write a function that reverses a number. The result must be a number.
+
+//12345 -> 54321 // Output must be a number
+
+
+function reversNumber(number){
+    var strNumber = "";
+    strNumber = strNumber + number;
+    
+    var strNumberReverse = "";
+  
+  for (var i = strNumber.length-1; i >= 0; i--){
+      strNumberReverse+=strNumber[i];
+      }
+  
+  return parseInt (strNumberReverse, 10);
+}
+
+console.log(reversNumber(5757575575));
