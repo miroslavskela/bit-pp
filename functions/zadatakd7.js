d14f9529b922936e36b3008c6f7b1a6d502d4e45
mikiskela <mikiskela1992@gmail.com>
2018-02-13 23:04:13 +0100

    functions 6 sedmi zadatak

--- functions/zadatakd7.js
+++ functions/zadatakd7.js
@@ -1,11 +1,19 @@
 //Write a function to find the maximum element in array of numbers. Filter out all non-number elements.
 
 'use strict';
-var myArr = [132, 2123, 23, 14, 421415, 36, 7];
+var myArr = [132, 2123,null, 23, 14, 421415, 36, 7];
 
 function maxElement(arr) {
-
-    var max = Math.max(myArr);
-    return max;
+var max = 0;
+for(var i = 0; i < arr.length; i++){
+    
+        if (typeof arr[i] === "number" && arr[i]>max) {
+            var max = arr[i];
+        }else {
+            var max = max;
+        }
+    
+}return max;
+   
 }
 console.log(maxElement(myArr));
